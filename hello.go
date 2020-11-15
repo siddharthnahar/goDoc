04eb086b5c5f3f7478a6294d2666d0ebaab01d75
Siddharth Nahar <siddharth.nahar.7@gmail.com>
2020-11-15 16:24:08 +0000

    first commit

---
+++ hello.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+
+	"log"
+)
+
+func main() {
+	//Set the properties of the predefined logger, including
+	//the log entry prefix and a flag to disable printing
+	//the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	//Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+	//if an error was returned, print it to the console and
+	//exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If no error was returned, print the returned message
+	//to the console
+	fmt.Println(messages)
+}
